944f5e86c307cc31ca8f0a306a5e4f672966755b
GlenKurio <gleb.kurip@gmail.com>
2023-12-18 19:22:59 -0600

    build/design: submit car delete action modal design

--- src/components/host/HostedCars/HostedCarsCard.jsx
+++ src/components/host/HostedCars/HostedCarsCard.jsx
@@ -91,8 +91,35 @@ function HostedCarsCard({ car }) {
 
       {isOpenModal && (
         <Modal setIsOpenModal={setIsOpenModal}>
-          <h2>Are you sure You want to delete this car?</h2>
-          <button onClick={handleClick}>DELETE</button>
+          <div className="flex flex-col gap-8">
+            <h2 className="text-2xl font-bold">
+              Are you sure You want to delete this car?
+            </h2>
+            <h2 className="text-xl font-semibold text-rose-500">
+              You wont be able to undo this action
+            </h2>
+
+            <button
+              onClick={handleClick}
+              className="p-2 flex justify-center items-center gap-2 text-rose-700 bg-rose-200 rounded-sm active:scale-95 transition-all duration-200 hover:bg-rose-300 hover:text-rose-800"
+            >
+              <span>Delete</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            </button>
+          </div>
         </Modal>
       )}
     </>
